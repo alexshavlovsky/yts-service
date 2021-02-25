c333a9e4d3be7c56f59f549af482f1ae397f55e4
alexshavlovsky <ctzn@tut.by>
2021-02-25 19:32:53 +0300

    initial commit

---
+++ src/main/java/com/ctzn/ytsservice/YtsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ctzn.ytsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctzn/ytsservice/YtsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctzn.ytsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

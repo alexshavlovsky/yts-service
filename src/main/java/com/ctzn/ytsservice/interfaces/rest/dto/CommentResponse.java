2fbeb5d2a4f5b6bb86793271cca4e92c0c62467c
alexshavlovsky <alexshavlovsky1@gmail.com>
2021-03-04 23:24:31 +0300

    implement h2 native full text search

--- src/main/java/com/ctzn/ytsservice/interfaces/rest/dto/CommentResponse.java
+++ src/main/java/com/ctzn/ytsservice/interfaces/rest/dto/CommentResponse.java
@@ -7,14 +7,14 @@ import java.util.Date;
 @Data
 public class CommentResponse {
     public String commentId;
-    public String videoId;
+    public String video;
     public String authorText;
     public String channelId;
     public String publishedTimeText;
     public String text;
     public int likeCount;
     public int replyCount;
-    public String parentId;
+    public String parent;
     public Date createdDate;
     public Date lastUpdatedDate;
 }

4a1c21afe8b7bd40dc6328002f79efe81929b38a
alexshavlovsky <alexshavlovsky1@gmail.com>
2021-04-06 22:42:41 +0300

    include a static angular client

---
+++ src/main/java/com/ctzn/ytsservice/interfaces/web/HomeController.java
@@ -0,0 +1,15 @@
+package com.ctzn.ytsservice.interfaces.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController { // forward all routes "/*" except "/*.*" to index.html
+
+    @GetMapping("/{angularRoute:^[^.]+$}")
+    public String index(@PathVariable String angularRoute) {
+        return "forward:/index.html";
+    }
+
+}
